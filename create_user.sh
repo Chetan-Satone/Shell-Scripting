e9840129f3c67cda4e7089b0cac8f01222c25a9e
Chetan Satone <102713932+Chetan-Satone@users.noreply.github.com>
2025-02-26 22:47:32 +0530

    Create create_user.sh

---
+++ create_user.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+read -p "Enter your name" username
+read -p "Enter your designation" designation
+read -p "Enter company name" company
+
+
+echo "you entered $username"
+echo "your designation $designation"
+echo "your company is $company"
+
+sudo useradd -m $username
+
+echo "New user Added"
